d365964535e46db24d79c1036fb5328fb67a213d
halodong <drl990114@gmail.com>
2023-05-25 23:10:52 +0800

    fix: ci error

--- src-tauri/src/setup.rs
+++ src-tauri/src/setup.rs
@@ -1,21 +1,15 @@
-use tauri::{utils::config::WindowUrl, window::WindowBuilder, App, TitleBarStyle, Theme};
+use tauri::{utils::config::WindowUrl, window::WindowBuilder, App, Theme};
 
 pub fn init(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
     let app = app.handle();
 
     tauri::async_runtime::spawn(async move {
-        let mut main_win = WindowBuilder::new(&app, "linebyline", WindowUrl::App("index.html".into()))
+        let main_win = WindowBuilder::new(&app, "linebyline", WindowUrl::App("index.html".into()))
             .title("linebyline")
             .resizable(true)
             .fullscreen(false)
             .theme(Theme::Light.into()) // TODO theme selected and setting
-            .hidden_title(false)
             .inner_size(800.0, 600.0);
-
-        #[cfg(target_os = "macos")]
-        {
-            // main_win = main_win.title_bar_style(TitleBarStyle::Overlay);
-        }
 
         main_win.build().unwrap();
     });
