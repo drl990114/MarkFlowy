5083094e984e21e36030abd66fcd805f14901b3b
halodong <drl990114@gmail.com>
2023-07-03 22:58:15 +0800

    feat: display app config file path

--- src-tauri/src/app/conf.rs
+++ src-tauri/src/app/conf.rs
@@ -1,7 +1,7 @@
+use crate::fc::{create_file, exists};
 use serde_json::Value;
 use std::{collections::BTreeMap, path::PathBuf};
-use tauri::{Manager, Theme};
-use crate::fc::{create_file, exists};
+use tauri::Theme;
 
 macro_rules! pub_struct {
   ($name:ident {$($field:ident: $t:ty,)*}) => {
@@ -13,124 +13,123 @@ macro_rules! pub_struct {
 }
 
 pub_struct!(AppConf {
-  theme: String,
-  language: String,
-  extensions_chatgpt_apikey: String,
+    theme: String,
+    language: String,
+    extensions_chatgpt_apikey: String,
 });
 
 pub const APP_CONF_PATH: &str = "linebyline.conf.json";
 
 pub fn app_root() -> PathBuf {
-  tauri::api::path::home_dir().unwrap().join(".linebyline")
+    tauri::api::path::home_dir().unwrap().join(".linebyline")
 }
 
 impl AppConf {
-  pub fn new() -> Self {
-    Self {
-      theme: "light".to_string(),
-      language: "en".to_string(),
-      extensions_chatgpt_apikey: "".to_string(),
+    pub fn new() -> Self {
+        Self {
+            theme: "light".to_string(),
+            language: "en".to_string(),
+            extensions_chatgpt_apikey: "".to_string(),
+        }
     }
-  }
 
-  pub fn file_path() -> PathBuf {
-    app_root().join(APP_CONF_PATH)
-  }
+    pub fn file_path() -> PathBuf {
+        app_root().join(APP_CONF_PATH)
+    }
 
-  pub fn read() -> Self {
-    match std::fs::read_to_string(Self::file_path()) {
-      Ok(v) => {
-        if let Ok(v2) = serde_json::from_str::<AppConf>(&v) {
-          v2
+    pub fn read() -> Self {
+        match std::fs::read_to_string(Self::file_path()) {
+            Ok(v) => {
+                if let Ok(v2) = serde_json::from_str::<AppConf>(&v) {
+                    v2
+                } else {
+                    Self::default()
+                }
+            }
+            Err(err) => Self::default(),
+        }
+    }
+
+    pub fn write(self) -> Self {
+        let path = &Self::file_path();
+        if !exists(path) {
+            create_file(path).unwrap();
+        }
+        if let Ok(v) = serde_json::to_string_pretty(&self) {
+            std::fs::write(path, v).unwrap_or_else(|err| {
+                Self::default().write();
+            });
         } else {
-          Self::default()
         }
-      }
-      Err(err) => {
-        Self::default()
-      }
-    }
-  }
-
-  pub fn write(self) -> Self {
-    let path = &Self::file_path();
-    if !exists(path) {
-      create_file(path).unwrap();
-    }
-    if let Ok(v) = serde_json::to_string_pretty(&self) {
-      std::fs::write(path, v).unwrap_or_else(|err| {
-        Self::default().write();
-      });
-    } else {
-    }
-    self
-  }
-
-  pub fn amend(self, json: Value) -> Self {
-    let val = serde_json::to_value(&self).unwrap();
-    let mut config: BTreeMap<String, Value> = serde_json::from_value(val).unwrap();
-    let new_json: BTreeMap<String, Value> = serde_json::from_value(json).unwrap();
-
-    for (k, v) in new_json {
-      config.insert(k, v);
-    }
-
-    match serde_json::to_string_pretty(&config) {
-      Ok(v) => match serde_json::from_str::<AppConf>(&v) {
-        Ok(v) => v,
-        Err(err) => {
-          self
-        }
-      },
-      Err(err) => {
         self
-      }
     }
-  }
 
-  pub fn get_theme() -> String {
-    Self::read().theme.to_lowercase()
-  }
+    pub fn amend(self, json: Value) -> Self {
+        let val = serde_json::to_value(&self).unwrap();
+        let mut config: BTreeMap<String, Value> = serde_json::from_value(val).unwrap();
+        let new_json: BTreeMap<String, Value> = serde_json::from_value(json).unwrap();
 
-  pub fn theme_mode() -> Theme {
-    match Self::get_theme().as_str() {
-      "system" => match dark_light::detect() {
-        dark_light::Mode::Dark => Theme::Dark,
-        dark_light::Mode::Light => Theme::Light,
-        dark_light::Mode::Default => Theme::Light,
-      },
-      "dark" => Theme::Dark,
-      _ => Theme::Light,
+        for (k, v) in new_json {
+            config.insert(k, v);
+        }
+
+        match serde_json::to_string_pretty(&config) {
+            Ok(v) => match serde_json::from_str::<AppConf>(&v) {
+                Ok(v) => v,
+                Err(err) => self,
+            },
+            Err(err) => self,
+        }
     }
-  }
 
-  pub fn theme_check(self, mode: &str) -> bool {
-    self.theme.to_lowercase() == mode
-  }
+    pub fn get_theme() -> String {
+        Self::read().theme.to_lowercase()
+    }
+
+    pub fn theme_mode() -> Theme {
+        match Self::get_theme().as_str() {
+            "system" => match dark_light::detect() {
+                dark_light::Mode::Dark => Theme::Dark,
+                dark_light::Mode::Light => Theme::Light,
+                dark_light::Mode::Default => Theme::Light,
+            },
+            "dark" => Theme::Dark,
+            _ => Theme::Light,
+        }
+    }
+
+    pub fn theme_check(self, mode: &str) -> bool {
+        self.theme.to_lowercase() == mode
+    }
 }
 
 impl Default for AppConf {
-  fn default() -> Self {
-    Self::new()
-  }
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 pub mod cmd {
-  use super::AppConf;
-  use tauri::{command, AppHandle};
+    use super::AppConf;
+    use tauri::{command, AppHandle};
 
-  #[command]
-  pub fn get_app_conf() -> AppConf {
-    AppConf::read()
-  }
+    #[command]
+    pub fn get_app_conf_path() -> String {
+        AppConf::file_path().to_str().unwrap().to_string()
+    }
 
-  #[command]
-  pub fn reset_app_conf() -> AppConf {
-    AppConf::default().write()
-  }
+    #[command]
+    pub fn get_app_conf() -> AppConf {
+        AppConf::read()
+    }
 
-  #[command]
-  pub fn save_app_conf(_app: AppHandle, data: serde_json::Value) {
-    AppConf::read().amend(serde_json::json!(data)).write();
-  }
+    #[command]
+    pub fn reset_app_conf() -> AppConf {
+        AppConf::default().write()
+    }
+
+    #[command]
+    pub fn save_app_conf(_app: AppHandle, data: serde_json::Value) {
+        AppConf::read().amend(serde_json::json!(data)).write();
+    }
 }
