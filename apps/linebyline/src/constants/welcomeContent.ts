5f3f2690d8ceb0e1ed19611fc49a9ed4f9554a9b
halodong <drl990114@gmail.com>
2023-07-06 22:31:35 +0800

    feat: code select menu (#144)

--- apps/linebyline/src/constants/welcomeContent.ts
+++ apps/linebyline/src/constants/welcomeContent.ts
@@ -1,6 +1,10 @@
 const WELCOMECONTENT = `
 # Welcome linebyline
 
+\`\`\`js
+console.log('hello world')
+\`\`\`
+
 ## Feature
 
 - **Super lightweight**. linebyline is based on tauri, which ensures ultra-lightweight and fast performance experience.
@@ -9,6 +13,8 @@ const WELCOMECONTENT = `
 ## Contribute
 
 The current linebyline is still in its infancy, and there may be some bad experiences or bugs, for which I am sorry. All partners who are interested or encounter usage problems are welcome to submit [issues](https://github.com/linebyline-group/linebyline/issues/new) or [PR](https://github.com/linebyline-group/linebyline/compare) to participate in this project.
+
+---
 
 ## Table
 
