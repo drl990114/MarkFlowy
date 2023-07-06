f05ba0a6a3d7bc4b51bd1908b372db06d1dbd977
halodong <drl990114@gmail.com>
2023-07-06 22:31:35 +0800

    feat: code select menu (#144)

--- packages/editor/src/extensions/CodeMIrror/codemirror-extension.ts
+++ packages/editor/src/extensions/CodeMIrror/codemirror-extension.ts
@@ -3,8 +3,13 @@ import type Token from 'markdown-it/lib/token'
 
 import type { NodeSerializerOptions } from '../../transform'
 import { ParserRuleType } from '../../transform'
+import type { EditorState } from 'remirror'
+import { findParentNodeOfType } from '@remirror/core'
+import { Decoration, DecorationSet } from '@remirror/pm/view'
+import createCodeMirrorMenuDecorations from './codemirror-lang-menu'
 
-const fakeIndentedLanguage = 'indent-code'
+export const fakeIndentedLanguage = 'indent-code'
+
 export class LineCodeMirrorExtension extends CodeMirrorExtension {
   public fromMarkdown() {
     return [
@@ -46,5 +51,28 @@ export class LineCodeMirrorExtension extends CodeMirrorExtension {
       state.closeBlock(node)
     }
     state.ensureNewLine()
+  }
+
+  createDecorations(state: EditorState): DecorationSet {
+    const found = findParentNodeOfType({ types: this.type, selection: state.selection })
+    if (!found) {
+      return DecorationSet.empty
+    }
+
+    const languages = this.options.languages
+    if (!languages || languages.length <= 1) {
+      return DecorationSet.empty
+    }
+
+    const { create, destroy } = createCodeMirrorMenuDecorations(found)
+
+    const deco = Decoration.widget(found.pos, create, {
+      ignoreSelection: true,
+      stopEvent: () => true,
+      key: 'language-menu',
+      destroy,
+    })
+
+    return DecorationSet.create(state.doc, [deco])
   }
 }
