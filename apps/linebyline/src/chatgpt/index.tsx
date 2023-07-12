7bbb30d179cd0b655bf8f22459695d345baf98d8
halodong <drl990114@gmail.com>
2023-07-12 20:41:04 +0800

    feat: chatgpt optimization close #161, #162,  #164

--- apps/linebyline/src/chatgpt/index.tsx
+++ apps/linebyline/src/chatgpt/index.tsx
@@ -1,7 +1,7 @@
 import { Input } from '@mui/material'
 import Button from '@mui/material/Button'
 import { emit } from '@tauri-apps/api/event'
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import ReactLoading from 'react-loading'
 import { parseChatList } from './parseChatList'
 import { BottomBar, Container, ListContainer } from './styles'
@@ -20,13 +20,31 @@ const ChatList: React.FC<ChatListProps> = (props) => {
   const apiKey = settingData[SettingKeys.chatgpt]
   const [askInput, setAskInput] = useState('')
   const { addOpenedFile, setActiveId } = useEditorStore()
+  const listRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (listRef.current) {
+      listRef.current.scrollTop = listRef.current.scrollHeight
+    }
+  }, [chatList.length])
 
   const handleSubmit = useCallback(() => {
-    if (!apiKey)
-      return
+    if (!apiKey) return
 
     addChat(askInput, apiKey)
-  }, [askInput, apiKey])
+    setAskInput('')
+  }, [apiKey, addChat, askInput])
+
+  const handleKeydown: React.KeyboardEventHandler<HTMLInputElement> = useCallback(
+    (event) => {
+      if (!event.shiftKey && event.keyCode === 13) {
+        handleSubmit()
+        event.preventDefault()
+        return false
+      }
+    },
+    [handleSubmit],
+  )
 
   const exportChats = useCallback(() => {
     const content = parseChatList(chatList)
@@ -35,10 +53,7 @@ const ChatList: React.FC<ChatListProps> = (props) => {
     setActiveId(gptNotesFile.id)
   }, [chatList, addOpenedFile, setActiveId])
 
-  const openSettingWindow = useCallback(
-    () => emit(EVENT.open_window_setting),
-    [],
-  )
+  const openSettingWindow = useCallback(() => emit(EVENT.open_window_setting), [])
 
   return (
     <Container {...props}>
@@ -48,9 +63,8 @@ const ChatList: React.FC<ChatListProps> = (props) => {
           <i className="icon ri-file-download-line" onClick={exportChats} />
         </div>
       </div>
-      <div className="content">
-        {chatList.length > 0
-          ? (
+      <div className="content" ref={listRef}>
+        {chatList.length > 0 ? (
           <ListContainer>
             {chatList.map((chat) => {
               return (
@@ -62,10 +76,7 @@ const ChatList: React.FC<ChatListProps> = (props) => {
                         <span>You</span>
                       </div>
                       <div>
-                        <i
-                          className="icon ri-delete-bin-line"
-                          onClick={() => delChat(chat.id)}
-                        />
+                        <i className="icon ri-delete-bin-line" onClick={() => delChat(chat.id)} />
                       </div>
                     </div>
                     <p>{chat.question}</p>
@@ -75,57 +86,49 @@ const ChatList: React.FC<ChatListProps> = (props) => {
                       <i className="ri-openai-fill item-icon" />
                       <span>ChatGPT</span>
                     </div>
-                    {chat.status === 'pending'
-                      ? (
+                    {chat.status === 'pending' ? (
                       <ReactLoading
                         type="bubbles"
                         width={35}
                         height={35}
                         color={themeColors.accentColor}
                       />
-                        )
-                      : chat.status === 'error'
-                        ? (
+                    ) : chat.status === 'error' ? (
                       <div>
-                        request error, please check your ApiKey is it right or
-                        not <a onClick={openSettingWindow}>setting</a>
+                        request error, please check your ApiKey is it right or not{' '}
+                        <a onClick={openSettingWindow}>setting</a>
                       </div>
-                          )
-                        : (
+                    ) : (
                       <p>{chat.answer}</p>
-                          )}
+                    )}
                   </div>
                 </div>
               )
             })}
           </ListContainer>
-            )
-          : (
+        ) : (
           <div className="introduction">
             <div className="introduction-title">
-              <i className="ri-flashlight-line" />Capabilities
+              <i className="ri-flashlight-line" />
+              Capabilities
             </div>
             <div className="introduction-item">
               One-click export of conversation content as md file
             </div>
           </div>
-            )}
+        )}
       </div>
       <BottomBar>
         <Input
           className="input"
           value={askInput}
           placeholder="input question"
+          onKeyDown={handleKeydown}
           onChange={(event) => {
             setAskInput(event.target.value)
           }}
         />
-        <Button
-          className="submit"
-          size="small"
-          variant="contained"
-          onClick={handleSubmit}
-        >
+        <Button className="submit" size="small" variant="contained" onClick={handleSubmit}>
           submit
         </Button>
       </BottomBar>
