f976c43bab0c5fd8c6719d11878f66f48517ad1d
halodong <drl990114@gmail.com>
2023-07-16 00:17:26 +0800

    refactor: editor offset prop

--- packages/editor/src/components/WysiwygEditor/index.tsx
+++ packages/editor/src/components/WysiwygEditor/index.tsx
@@ -1,22 +1,30 @@
 import { Remirror } from '@remirror/react'
-import type { FC } from 'react'
+import { type FC, createContext } from 'react'
 import Text from '../Text'
 import Wrapper from '../Wrapper'
 import { createWysiwygDelegate } from './delegate'
 import type { EditorDelegate } from '../../../types'
 import TableToolbar from '../../toolbar/TableToolbar'
 
+export const OffsetContext = createContext({ top: 0, left: 0 })
+
 const WysiwygEditor: FC<WysiwygEditorProps> = (props) => {
-  const { content, hooks, delegate } = props
+  const { content, hooks, delegate, offset } = props
 
   const editorDelegate = delegate ?? createWysiwygDelegate()
 
   return (
     <Wrapper>
-      <Remirror manager={editorDelegate.manager} initialContent={editorDelegate.stringToDoc(content)} hooks={hooks}>
-        <TableToolbar />
-        <Text className="w-full markdown-body" />
-      </Remirror>
+      <OffsetContext.Provider value={offset || { top: 0, left: 0 }}>
+        <Remirror
+          manager={editorDelegate.manager}
+          initialContent={editorDelegate.stringToDoc(content)}
+          hooks={hooks}
+        >
+          <TableToolbar />
+          <Text className="w-full markdown-body" />
+        </Remirror>
+      </OffsetContext.Provider>
     </Wrapper>
   )
 }
@@ -29,6 +37,10 @@ interface WysiwygEditorProps {
   file: Global.IFile
   content: string
   active: boolean
+  offset?: {
+    top: number
+    left: number
+  }
   hooks?: (() => void)[]
   delegate?: EditorDelegate
 }
--- packages/editor/src/toolbar/TableToolbar/TableBar.tsx
+++ packages/editor/src/toolbar/TableToolbar/TableBar.tsx
@@ -1,5 +1,7 @@
+import { OffsetContext } from '../../components/WysiwygEditor'
 import type { UseMultiPositionerReturn } from '@remirror/react'
 import { useCommands } from '@remirror/react'
+import { useContext } from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -10,15 +12,16 @@ const Container = styled.div`
 function TableBar(props: TableBarProps) {
   const { positioner } = props
   const commands = useCommands()
-
+  const offset = useContext(OffsetContext)
   const { ref, key, x, y } = positioner
+
   return (
     <Container
       key={key}
       ref={ref}
       style={{
-        left: x + 16,
-        top: y + 32 - 26,
+        left: x + offset.left,
+        top: y + offset.top - 26,
         width: 20,
         height: 20,
       }}
