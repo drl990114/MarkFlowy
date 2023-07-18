e507cabbc757c2762cf77c83cc281a0100f785e7
halodong <drl990114@gmail.com>
2023-07-18 21:36:20 +0800

    chore: omission

--- apps/linebyline/src/hooks/index.ts
+++ apps/linebyline/src/hooks/index.ts
@@ -1,4 +1,3 @@
-export { default as useFloatingLinkState } from '@linebyline/editor/src/hooks/useFloatingLinkState'
 export { default as useGlobalCacheData } from './useCacheData'
 export { default as useGlobalSettingData } from './useSettingData'
 export { default as useGlobalTheme } from './useTheme'
