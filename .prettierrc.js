4a3a260e38bee20dd73a5a65e50dc92d4235c6b2
halodong <drl990114@gmail.com>
2023-07-23 21:01:39 +0800

    chore: add prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/** @type {import('prettier').Options} */
+const config = {
+  jsxSingleQuote: true,
+  printWidth: 100,
+  proseWrap: 'never',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
+
+module.exports = config;
