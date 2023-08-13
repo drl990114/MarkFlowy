b0d55e8b385229fab7033dc9dee9e0d45882f6b4
halodong <drl990114@gmail.com>
2023-08-13 22:37:24 +0800

    test: helper/common

--- apps/linebyline/src/helper/test/common.test.ts
+++ apps/linebyline/src/helper/test/common.test.ts
@@ -1,22 +1,15 @@
 import { describe, it, expect } from 'vitest'
-import { getFileNameFromPath, isMdFile } from '../filesys'
+import { once } from '../common'
 
-describe('test helper/filesys ', () => {
-  it('getFileNameFromPath', () => {
-    const macPath = '/path/to/myfile.txt'
-    const winPath = 'C:\\path\\to\\myfile.txt'
-
-    expect(getFileNameFromPath(macPath)).toBe('myfile.txt')
-    expect(getFileNameFromPath(winPath)).toBe('myfile.txt')
-  })
-
-  it('isMdFile', () => {
-    const macPath = '/path/to/myfile.md'
-    const winPath = 'C:\\path\\to\\myfile.md'
-    const otherPath = 'C:\\path\\to\\myfile.txt'
-
-    expect(isMdFile(macPath)).toBe(true)
-    expect(isMdFile(winPath)).toBe(true)
-    expect(isMdFile(otherPath)).toBe(false)
+describe('test helper/common ', () => {
+  it('once', () => {
+    let count = 0
+    const fn = once(() => {
+      count++
+    })
+    fn()
+    fn()
+    fn()
+    expect(count).toBe(1)
   })
 })
