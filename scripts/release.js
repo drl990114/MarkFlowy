9fe4b08be7082c0f60184cd2d8a88fcd9f22ed03
halodong <drl990114@gmail.com>
2023-08-16 23:35:11 +0800

    chore: update release script

--- scripts/release.js
+++ scripts/release.js
@@ -3,15 +3,50 @@ const { execSync } = require('child_process')
 const fs = require('fs')
 
 const cwd = process.cwd()
-const tauriConfFilePath = path.resolve(cwd, './src-tauri/tauri.conf.json')
+const tauriConfFilePath = path.resolve(cwd, './apps/desktop/src-tauri/tauri.conf.json')
 const tauriConf = require(tauriConfFilePath)
 
-const nextVersion = process.argv[2] || getNextVersion(tauriConf.package.version)
+const nextVersion = process.argv[2] ? extractVersion(process.argv[2]) : getNextVersion(tauriConf.package.version)
+const allWorkspaces = getWorkspacePackageByDirName('apps').concat(getWorkspacePackageByDirName('packages'))
+
+allWorkspaces.forEach((workspace) => {
+  const packageContent = require(workspace.packageFilePath)
+  packageContent.version = nextVersion
+  fs.writeFileSync(workspace.packageFilePath, JSON.stringify(packageContent, null, 2))
+})
+
+updateVersion()
+
+execSync(`git add . && git commit -m "chore: bump version to ${nextVersion}" && git push`)
+execSync(`git tag -a v${nextVersion} -m "v${nextVersion}" && git push linebyline v${nextVersion}`)
+
+
+// helpers -----------------------------
+function extractVersion (string) {
+  return string.match(/\d+\.\d+\.\d+/g)[0]
+}
+
+function isDirectory(path) {
+  return fs.statSync(path).isDirectory()
+}
+
+function getWorkspacePackageByDirName(dirName) {
+  const appsWorkspaces = []
+
+  fs.readdirSync(path.resolve(cwd, `./${dirName}`)).forEach((file) => {
+    if (isDirectory(path.resolve(cwd, `./${dirName}/${file}`))) {
+      appsWorkspaces.push({
+        packageFilePath: path.resolve(cwd, `./${dirName}/${file}/package.json`)
+      })
+    }
+  })
+
+  return appsWorkspaces
+}
 
 function updateVersion() {
   tauriConf.package.version = nextVersion
   fs.writeFileSync(tauriConfFilePath, JSON.stringify(tauriConf, null, 2))
-  execSync('git add . && git commit -m "chore: bump version" && git push')
 }
 
 function getNextVersion(version) {
@@ -25,6 +60,4 @@ function getNextVersion(version) {
   }
 }
 
-updateVersion()
 
-execSync(`git tag -a v${nextVersion} -m "v${nextVersion}" && git push linebyline v${nextVersion}`)
