6a6e1f6eb25fe90fb936ba0c4e9a172d7ffba0f5
halodong <drl990114@gmail.com>
2023-08-30 21:23:58 +0800

    test(utils): add test for is_md_file_name

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -5,3 +5,14 @@ pub fn is_md_file_name(file_name: &str) -> bool {
 
     reg.is_match(file_name)
 }
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_md_file_name() {
+        assert!(super::is_md_file_name("test.md"));
+        assert!(!super::is_md_file_name("test.txt"));
+        assert!(!super::is_md_file_name("test.mdx"));
+    }
+}
