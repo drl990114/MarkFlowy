219bf02a298850dd5400d3004d45acc5f183a056
halodong <drl990114@gmail.com>
2023-08-31 21:03:07 +0800

    feat: add ErrorBoundary

--- packages/editor/src/components/WysiwygEditor/index.tsx
+++ packages/editor/src/components/WysiwygEditor/index.tsx
@@ -5,8 +5,9 @@ import Wrapper from '../Wrapper'
 import { createWysiwygDelegate } from './delegate'
 import type { EditorDelegate } from '../../types'
 import TableToolbar from '../../toolbar/TableToolbar'
-import { ProsemirrorDevTools } from "@remirror/dev"
-import React from "react"
+import { ProsemirrorDevTools } from '@remirror/dev'
+import React from 'react'
+import ErrorBoundary from '../ErrorBoundary'
 
 export const OffsetContext = createContext({ top: 0, left: 0 })
 
@@ -15,21 +16,30 @@ const WysiwygEditor: FC<WysiwygEditorProps> = (props) => {
 
   const editorDelegate = delegate ?? createWysiwygDelegate()
 
+  let initialContent
+  try {
+    initialContent = editorDelegate.stringToDoc(content)
+  } catch (error) {
+    return <ErrorBoundary hasError error={error}/>
+  }
+
   return (
-    <Wrapper>
-      <OffsetContext.Provider value={offset || { top: 0, left: 0 }}>
-        <Remirror
-          manager={editorDelegate.manager}
-          initialContent={editorDelegate.stringToDoc(content)}
-          hooks={hooks}
-        >
-          <TableToolbar />
-          {wysiwygToolBar || null}
-          <Text className="w-full markdown-body" />
-          {isTesting ? <ProsemirrorDevTools /> : null}
-        </Remirror>
-      </OffsetContext.Provider>
-    </Wrapper>
+    <ErrorBoundary>
+      <Wrapper>
+        <OffsetContext.Provider value={offset || { top: 0, left: 0 }}>
+          <Remirror
+            manager={editorDelegate.manager}
+            initialContent={initialContent}
+            hooks={hooks}
+          >
+            <TableToolbar />
+            {wysiwygToolBar || null}
+            <Text className='w-full markdown-body' />
+            {isTesting ? <ProsemirrorDevTools /> : null}
+          </Remirror>
+        </OffsetContext.Provider>
+      </Wrapper>
+    </ErrorBoundary>
   )
 }
 
