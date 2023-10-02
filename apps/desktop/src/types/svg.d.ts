5a82cfbd2e872760928b73a283c14e92e2a3c7b2
halodong <drl990114@gmail.com>
2023-10-02 18:50:33 +0800

    chore: bump vite-plugin-svgr to 4.1.0

--- apps/desktop/src/types/svg.d.ts
+++ apps/desktop/src/types/svg.d.ts
@@ -1,6 +1,7 @@
-declare module '*.svg' {
-  import * as React from 'react'
-  export const ReactComponent: React.FunctionComponent<
+declare module '*.svg?react' {
+  import type * as React from 'react'
+  const ReactComponent: React.FunctionComponent<
     React.SVGProps<SVGSVGElement> & { title?: string }
   >
+  export default ReactComponent
 }
