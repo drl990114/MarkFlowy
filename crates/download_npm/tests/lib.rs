428e48aa62e1a6e020deb2097d82d48f707ddefb
drl990114 <drl990114@gmail.com>
2023-10-23 23:24:35 +0800

    feat(npm_download): support untar

--- crates/download_npm/tests/lib.rs
+++ crates/download_npm/tests/lib.rs
@@ -1,13 +1,28 @@
 extern crate download_npm;
 
-const TEST_FOLDER: &'static str = "./tests/tmp";
 
 #[test]
 fn test_download() {
+  const TEST_FOLDER: &'static str = "./tests/tar";
   download_npm::download("react", download_npm::DownloadOptions {
-    dest_path: TEST_FOLDER.to_string()
+    dest_path: TEST_FOLDER.to_string(),
+    untar: false,
   }).unwrap();
 
   assert!(std::path::Path::new(TEST_FOLDER).exists());
   assert!(std::path::Path::new(TEST_FOLDER).join("react.tgz").exists());
+}
+
+
+#[test]
+fn test_download_untar() {
+  const TEST_FOLDER: &'static str = "./tests/untar";
+  download_npm::download("react", download_npm::DownloadOptions {
+    dest_path: TEST_FOLDER.to_string(),
+    untar: true,
+  }).unwrap();
+
+  assert!(std::path::Path::new(TEST_FOLDER).exists());
+  assert!(std::path::Path::new(TEST_FOLDER).join("react").join("package.json").exists());
+  assert!(std::path::Path::new(TEST_FOLDER).join("react").join("index.js").exists());
 }
