c763c0f59dc7fd9f584ac2257aaedf8cea06a4a9
drl990114 <drl990114@gmail.com>
2023-12-04 22:31:46 +0800

    test(download_npm): fix use async test

--- crates/download_npm/tests/lib.rs
+++ crates/download_npm/tests/lib.rs
@@ -1,26 +1,25 @@
 extern crate download_npm;
 
-
-#[test]
-fn test_download() {
+#[tokio::test]
+async fn test_download() {
   const TEST_FOLDER: &'static str = "./tests/tar";
-  download_npm::download("react", download_npm::DownloadOptions {
+  let _ =  download_npm::download("react", download_npm::DownloadOptions {
     dest_path: TEST_FOLDER.to_string(),
     untar: false,
-  }).unwrap();
+  }).await;
 
   assert!(std::path::Path::new(TEST_FOLDER).exists());
   assert!(std::path::Path::new(TEST_FOLDER).join("react.tgz").exists());
 }
 
 
-#[test]
-fn test_download_untar() {
+#[tokio::test]
+async fn test_download_untar() {
   const TEST_FOLDER: &'static str = "./tests/untar";
-  download_npm::download("markflowy-theme-template", download_npm::DownloadOptions {
+  let _ = download_npm::download("markflowy-theme-template", download_npm::DownloadOptions {
     dest_path: TEST_FOLDER.to_string(),
     untar: true,
-  }).unwrap();
+  }).await;
 
   assert!(std::path::Path::new(TEST_FOLDER).exists());
   assert!(std::path::Path::new(TEST_FOLDER).join("markflowy-theme-template").join("package.json").exists());
