768499dc77c08a69d8f9aa3374659a70640fa6dc
drl990114 <drl990114@gmail.com>
2024-02-08 16:40:37 +0800

    refactor: remove useless code

--- apps/desktop/src/context/index.ts
+++ apps/desktop/src/context/index.ts
@@ -1,9 +1,7 @@
 import type { MF_CONTEXT } from '@markflowy/types'
-import * as editor from './editor'
 import * as theme from './theme'
 
 const __MF__: MF_CONTEXT = Object.freeze({
-  editor,
   theme
 })
 
