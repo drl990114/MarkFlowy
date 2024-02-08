2700a872da616e6b4857b4af0e8196801f80bc23
drl990114 <drl990114@gmail.com>
2024-02-08 16:40:37 +0800

    refactor: remove useless code

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,9 +1,6 @@
 import type { MfTheme } from '@markflowy/theme'
 
 export type MF_CONTEXT = {
-  editor: {
-    getActiveEditorContent: () => string | null
-  }
   theme: {
     registerTheme: (theme: MfTheme) => void
   }
