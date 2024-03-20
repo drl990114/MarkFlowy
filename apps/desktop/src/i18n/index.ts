9dfc2353df00fa12a63b2e27c8b9023117dbfe0c
drl990114 <drl990114@gmail.com>
2024-03-20 22:18:17 +0800

    feat: add French translation support close #500

--- apps/desktop/src/i18n/index.ts
+++ apps/desktop/src/i18n/index.ts
@@ -3,19 +3,23 @@ import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import EN from '../../../../locales/en.json'
 import zhCN from '../../../../locales/zh-CN.json'
+import frFR from '../../../../locales/fr-FR.json'
 
 export const locales = {
   en: 'English',
   cn: '简体中文',
+  frFR: 'Français',
 }
 export const editorResources = {
   en: { translation: { ...EN.editor } },
   cn: { translation: { ...zhCN.editor } },
+  frFR: { translation: { ...frFR.editor } },
 }
 
 export const resources = {
   en: { translation: { ...EN } },
   cn: { translation: { ...zhCN } },
+  frFR: { translation: { ...frFR } },
 } as const
 
 export async function i18nInit(options?: InitOptions) {
