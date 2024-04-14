d4bbee8bd895e9ca2e5ac0269a099bf91b7d457f
drl990114 <drl990114@gmail.com>
2024-04-14 17:18:06 +0800

    fix: codemirror deps error

--- docs/src/util/slideshow.tsx
+++ docs/src/util/slideshow.tsx
@@ -1,69 +1,73 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
 const slides = [
-	{
-		name: 'Default',
-		src: '/img/index.png',
-		alt: 'MarkFlowy',
-	},
-	{
-		name: 'Dark Mode',
-		src: '/img/darkmode.png',
-		alt: 'MarkFlowy Dark Mode',
-	},
-	{
-		name: 'Chatgpt',
-		src: '/img/chatgpt.png',
-		alt: 'MarkFlowy Chatgpt Extension',
-	},
+  {
+    name: 'Default',
+    src: '/img/index.png',
+    alt: 'MarkFlowy',
+  },
+  {
+    name: 'Dark Mode',
+    src: '/img/darkmode.png',
+    alt: 'MarkFlowy Dark Mode',
+  },
+  {
+    name: 'Chatgpt',
+    src: '/img/chatgpt.png',
+    alt: 'MarkFlowy Chatgpt Extension',
+  },
 ]
 
 const SLIDE_INTERVAL = 2500
 
 export default function Slideshow() {
-	const [index, setIndex] = useState(0)
-	const [handle, setHandle] = useState<NodeJS.Timeout | undefined>()
+  const [index, setIndex] = useState(0)
+  const [handle, setHandle] = useState<NodeJS.Timeout | undefined>()
 
-	const destroyIntervalTimer = () => {
-		clearInterval(handle)
-		setHandle(undefined)
-	}
+  const destroyIntervalTimer = useCallback(() => {
+    clearInterval(handle)
+    setHandle(undefined)
+  }, [handle])
 
-	const createIntervalTimer = () => {
-		destroyIntervalTimer()
-		setHandle(setInterval(() => setIndex((i) => (i + 1) % slides.length), SLIDE_INTERVAL))
-	}
+  const createIntervalTimer = useCallback(() => {
+    destroyIntervalTimer()
+    setHandle(setInterval(() => setIndex((i) => (i + 1) % slides.length), SLIDE_INTERVAL))
+  }, [destroyIntervalTimer])
 
-	const handleSlideSelect = (idx) => {
-		createIntervalTimer()
-		setIndex(idx)
-	}
+  const handleSlideSelect = (idx) => {
+    createIntervalTimer()
+    setIndex(idx)
+  }
 
-	useEffect(() => {
-		createIntervalTimer()
-		return destroyIntervalTimer
-	}, [])
+  useEffect(() => {
+    createIntervalTimer()
+    return destroyIntervalTimer
+  }, [createIntervalTimer, destroyIntervalTimer])
 
-	return (
-		<>
-			<div className="slideshow-container">
-				{slides.map((e, i) => (
-					<div key={i} className={`slide${index === i ? ' active' : ''}`}>
-						<div className="slide-numbertext">
-							{i + 1} / {slides.length}
-						</div>
+  return (
+    <>
+      <div className='slideshow-container'>
+        {slides.map((e, i) => (
+          <div key={i} className={`slide${index === i ? ' active' : ''}`}>
+            <div className='slide-numbertext'>
+              {i + 1} / {slides.length}
+            </div>
 
-						<img src={e.src} alt={e.alt} />
-						<p className="slide-caption">{e.name}</p>
-					</div>
-				))}
-			</div>
+            <img src={e.src} alt={e.alt} />
+            <p className='slide-caption'>{e.name}</p>
+          </div>
+        ))}
+      </div>
 
-			<div className="slide-dots">
-				{slides.map((e, i) => (
-					<span onClick={() => handleSlideSelect(i)} key={e.name} className={`slide-dot${i === index ? ' active' : ''}`} />
-				))}
-			</div>
-		</>
-	)
+      <div className='slide-dots'>
+        {slides.map((e, i) => (
+          <span
+            onClick={() => handleSlideSelect(i)}
+            key={e.name}
+            className={`slide-dot${i === index ? ' active' : ''}`}
+          />
+        ))}
+      </div>
+    </>
+  )
 }
