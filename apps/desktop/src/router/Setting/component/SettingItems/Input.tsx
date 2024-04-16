9c5a08f34484684253b4876f37d268cb1f1da85f
drl990114 <drl990114@gmail.com>
2024-04-16 23:02:18 +0800

    perf: setting input callback debounce

--- apps/desktop/src/router/Setting/component/SettingItems/Input.tsx
+++ apps/desktop/src/router/Setting/component/SettingItems/Input.tsx
@@ -1,10 +1,11 @@
 import { Input } from 'zens'
-import { memo, useCallback } from 'react'
+import { memo, useCallback, useEffect, useState } from 'react'
 import type { SettingItemProps } from '.'
 import { SettingLabel } from './Label'
 import { SettingItemContainer } from './Container'
 import useAppSettingStore from '@/stores/useAppSettingStore'
 import appSettingService from '@/services/app-setting'
+import { debounce } from 'lodash'
 
 const InputSettingItem: React.FC<SettingItemProps<Setting.InputSettingItem>> = memo((
   props,
@@ -13,19 +14,30 @@ const InputSettingItem: React.FC<SettingItemProps<Setting.InputSettingItem>> = m
   const { settingData } = useAppSettingStore()
   const curValue = settingData[item.key] as unknown as string
 
-  const handleChange = useCallback(
-    (e: { target: { value: any } }) => {
-      const settingValue = e.target.value
-      appSettingService.writeSettingData(item, settingValue)
-    },
+  const [inputValue, setInputValue] = useState<string>(curValue)
+
+  useEffect(() => {
+    setInputValue(curValue)
+  }, [curValue])
+
+  const writeSettingData = useCallback(
+    debounce((value) => {
+      appSettingService.writeSettingData(item, value)
+    }, 1000),
     [item],
   )
+
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    setInputValue(value)
+    writeSettingData(value)
+  }, [writeSettingData])
 
   return (
     <SettingItemContainer>
       <SettingLabel item={item} />
       <Input
-        value={curValue}
+        value={inputValue}
         onChange={handleChange}
       />
     </SettingItemContainer>
