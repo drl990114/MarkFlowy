eab1abd49751ad14dd3ba4e8a7c05307ed2cd9f2
drl990114 <drl990114@gmail.com>
2024-04-21 21:20:00 +0800

    fix: toc refresh when active editor change

--- apps/desktop/src/components/EditorArea/index.tsx
+++ apps/desktop/src/components/EditorArea/index.tsx
@@ -1,7 +1,7 @@
 import Editor from './Editor'
 import { Container } from './styles'
-import { useEditorStore } from '@/stores'
-import { memo } from 'react'
+import { useCommandStore, useEditorStore } from '@/stores'
+import { memo, useEffect } from 'react'
 import EditorAreaTabs from './EditorAreaTabs'
 import { FindReplace } from '@/components/EditorArea/editorToolBar/FindReplace'
 import { EditorCount } from './editorToolBar/EditorCount'
@@ -9,6 +9,10 @@ import { EmptyState } from './EmptyState'
 
 function EditorArea() {
   const { opened, activeId } = useEditorStore()
+
+  useEffect(() => {
+    useCommandStore.getState().execute('app:toc_refresh')
+  }, [activeId])
 
   if (opened.length === 0) {
     return <EmptyState />
