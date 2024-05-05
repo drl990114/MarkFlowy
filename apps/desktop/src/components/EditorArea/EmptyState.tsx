a2298e4b7649e18293a705bd0478e2c231e10f74
drl990114 <drl990114@gmail.com>
2024-05-05 22:43:19 +0800

    fix: new files are not allowed when the workspace is empty

--- apps/desktop/src/components/EditorArea/EmptyState.tsx
+++ apps/desktop/src/components/EditorArea/EmptyState.tsx
@@ -1,25 +1,37 @@
+import { useOpen } from '@/hooks'
 import { addNewMarkdownFileEdit } from '@/services/editor-file'
+import { useEditorStore } from '@/stores'
 import { memo } from 'react'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
 
 export const EmptyState = memo(() => {
+  const { folderData } = useEditorStore()
   const { t } = useTranslation()
+  const { openFolderDialog } = useOpen()
 
   return (
     <Container className='w-full h-full'>
       <div>
         <p className='text-4xl font-bold text-gray-400'>{t('file.emptyOpened')}</p>
-        <a
-          onClick={() =>
-            addNewMarkdownFileEdit({
-              fileName: 'new-file.md',
-              content: '',
-            })
-          }
-        >
-          {t('action.create_file')}
-        </a>
+        {folderData?.length ? (
+          <a
+            onClick={() =>
+              addNewMarkdownFileEdit({
+                fileName: 'new-file.md',
+                content: '',
+              })
+            }
+          >
+            {t('action.create_file')}
+          </a>
+        ) : (
+          <a
+            onClick={openFolderDialog}
+          >
+           {t('file.openDir')}
+          </a>
+        )}
       </div>
     </Container>
   )
