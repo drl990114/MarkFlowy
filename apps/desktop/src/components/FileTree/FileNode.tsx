b6f4be248dd0efa04c2c1c14532e348ebacd99f2
drl990114 <drl990114@gmail.com>
2024-05-23 23:05:35 +0800

    feat: support move file or folder (#661)

--- apps/desktop/src/components/FileTree/FileNode.tsx
+++ apps/desktop/src/components/FileTree/FileNode.tsx
@@ -1,137 +1,161 @@
-import classNames from 'classnames'
-import type { FC, MouseEventHandler } from 'react'
-import { memo, useCallback, useEffect, useRef, useState } from 'react'
-import { FileNodeStyled } from './styles'
-import type { IFile } from '@/helper/filesys'
-import type { NewInputRef } from './NewFIleInput'
-import NewFileInput from './NewFIleInput'
-import bus from '@/helper/eventBus'
-import { useEditorStore } from '@/stores'
-import { EVENT } from '@/constants'
+import { createFile, type IFile } from '@/helper/filesys'
+import { NodeRendererProps } from 'react-arborist'
+import { NodeContainer } from './styles'
+import { invoke } from '@tauri-apps/api/core'
 import { showContextMenu } from '../UI/ContextMenu'
+import { nanoid } from 'nanoid'
+import { SimpleTree } from './SimpleTree'
+import NewFileInput from './NewFIleInput'
+import { useTranslation } from 'react-i18next'
 import NiceModal from '@ebay/nice-modal-react'
 import { MODAL_CONFIRM_ID } from '../Modal'
-import { useTranslation } from 'react-i18next'
+import { useEditorStore } from '@/stores'
 
-const FileNode: FC<FileNodeProps> = ({ item, level = 0, activeId, onSelect, open = false }) => {
-  const [isOpen, setIsOpen] = useState(open)
-  const newInputRef = useRef<NewInputRef>(null)
-  const { deleteNode } = useEditorStore()
+function FileNode({
+  style,
+  node,
+  dragHandle,
+  tree,
+  simpleTree,
+  setFolderData,
+}: NodeRendererProps<IFile> & {
+  simpleTree: SimpleTree<IFile>
+  setFolderData: any
+}) {
+  const indentSize = Number.parseFloat(`${style.paddingLeft || 0}`)
   const { t } = useTranslation()
-  const isActived = activeId === item.id
-  const isFolder = item.kind === 'dir'
+  const { deleteNode } = useEditorStore()
 
-  useEffect(() => {
-    setIsOpen(open)
-  }, [open])
+  const delFileHandler = () => {
+    deleteNode(node.data).then(() => {
+      simpleTree.drop({ id: node.id })
+      setFolderData(simpleTree.data)
+    })
+  }
 
-  const handleClick: MouseEventHandler = useCallback(
-    (e) => {
-      e.stopPropagation()
-      bus.emit(EVENT.sidebar_hide_new_input)
-      setIsOpen(!isOpen)
-    },
-    [isOpen],
-  )
-
-  const handleSelect: MouseEventHandler = useCallback(() => {
-    onSelect(item)
-  }, [item, onSelect])
-
-  const handleContextMenu = useCallback(
-    (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-      e.preventDefault()
-      e.stopPropagation()
-      const contextMenuNode = item
-
-      const newFileHandler = () => {
-        newInputRef.current?.show({ fileNode: contextMenuNode })
+  return (
+    <NodeContainer
+      style={style}
+      highlight={
+        !!(
+          tree.dragDestinationParent?.isAncestorOf(node) &&
+          tree.dragDestinationParent?.id !== tree.dragNode?.parent?.id
+        )
       }
+      selected={tree.selectedNodes.includes(node)}
+      onContextMenu={(e) => {
+        e.stopPropagation()
+        e.preventDefault()
+        const items = []
 
-      const delFileHandler = () => {
-        deleteNode(contextMenuNode)
-      }
+        if (node.parent) {
+          items.push({
+            label: t('contextmenu.explorer.add_file'),
+            value: 'new_file',
+            handler: () => {
+              if (node.parent) {
+                const data = { id: nanoid(), name: '', kind: 'pending' } as any
+                if (node.isInternal) {
+                  node.open()
+                }
+                simpleTree.create({
+                  parentId: node.parent.id,
+                  data,
+                  index: 0,
+                  nodeId: node.id,
+                  nodeType: node.isLeaf ? 'leaf' : 'internal',
+                })
+                tree.create({
+                  parentId: node.parent?.id,
+                  index: 0,
+                })
 
-      const contextMenuItems = [
-        {
-          value: 'add_file',
-          label: t('contextmenu.explorer.add_file'),
-          handler: () => {
-            newFileHandler()
-          },
-        },
-      ]
-
-      if (level !== 0) {
-        contextMenuItems.push({
-          value: item.kind === 'dir' ? 'delete_folder' : 'delete_file',
+                setFolderData(simpleTree.data)
+              }
+            },
+          })
+        }
+        items.push({
+          value: node.data.kind === 'dir' ? 'delete_folder' : 'delete_file',
           label:
-            item.kind === 'dir'
+            node.data.kind === 'dir'
               ? t('contextmenu.explorer.delete_folder')
               : t('contextmenu.explorer.delete_file'),
           handler: () => {
             NiceModal.show(MODAL_CONFIRM_ID, {
               title: t('confirm.delete.description', {
-                name: item.name,
-                something: item.kind === 'dir' ? t('common.folder') : t('common.file'),
+                name: node.data.name,
+                something: node.data.kind === 'dir' ? t('common.folder') : t('common.file'),
               }),
               onConfirm: delFileHandler,
             })
           },
         })
-      }
 
-      showContextMenu({
-        x: e.clientX,
-        y: e.clientY,
-        items: contextMenuItems,
-      })
-    },
-    [deleteNode, item, t, level],
-  )
+        showContextMenu({
+          x: e.clientX,
+          y: e.clientY,
+          items,
+        })
+      }}
+      onClick={(e) => {
+        if (e.shiftKey) {
+          return
+        }
+        node.isInternal && node.toggle()
+      }}
+      ref={dragHandle}
+    >
+      <div style={{ display: 'flex', paddingLeft: '6px' }}>
+        <div className='indentLines'>
+          {new Array(indentSize / 16).fill(0).map((_, index) => {
+            return <div key={index}></div>
+          })}
+        </div>
 
-  const nodeWrapperCls = classNames('file-node', {
-    'file-node--active': isActived,
-  })
+        {node.data.kind === 'pending' ? (
+          <NewFileInput
+            style={{ paddingTop: 0, paddingBottom: 0 }}
+            fileNode={node.data}
+            parentNode={node.parent?.data}
+            onCreate={async (file) => {
+              const targetFile = createFile({
+                path: file.path,
+                name: file.name,
+                content: '',
+                id: node.id,
+              })
 
-  const iconCls = 'file-icon'
+              console.log('onCreate', targetFile)
+              await invoke('write_file', {
+                filePath: targetFile.path,
+                content: targetFile.content,
+              })
 
-  return (
-    <FileNodeStyled onClick={handleClick} onContextMenu={handleContextMenu}>
-      <NewFileInput ref={newInputRef} autoFocus style={{ marginLeft: level * 16 + 6 }} />
-      <div
-        className={nodeWrapperCls}
-        style={{ paddingLeft: level * 16 + 6 }}
-        onClick={handleSelect}
-      >
-        {isFolder ? (
-          <i className={`${isOpen ? 'ri-folder-5-line' : 'ri-folder-3-line'} ${iconCls}`} />
-        ) : (
-          <i className={`ri-markdown-fill ${iconCls}`} />
-        )}
-        <div className='file-node__text'>{item.name}</div>
-      </div>
-      {isOpen &&
-        item.children &&
-        item.children.map((child) => (
-          <FileNode
-            key={child.name}
-            item={child}
-            level={level + 1}
-            activeId={activeId}
-            onSelect={onSelect}
+              simpleTree.update({
+                id: node.id,
+                changes: { ...node.data, ...targetFile },
+              })
+              setFolderData(simpleTree.data)
+            }}
+            onCancel={() => {
+              simpleTree.drop({ id: node.id })
+              setFolderData(simpleTree.data)
+            }}
           />
-        ))}
-    </FileNodeStyled>
+        ) : (
+          <>
+            {node.data?.kind === 'dir' ? (
+              <i className={`${node.isOpen ? 'ri-folder-5-line' : 'ri-folder-3-line'} file-icon`} />
+            ) : (
+              <i className={`ri-markdown-fill file-icon`} />
+            )}
+            {node.data.name}
+          </>
+        )}
+      </div>
+    </NodeContainer>
   )
 }
 
-interface FileNodeProps {
-  item: IFile
-  level: number
-  open?: boolean
-  activeId?: string
-  onSelect: (file: IFile) => void
-}
-
-export default memo(FileNode)
+export default FileNode
