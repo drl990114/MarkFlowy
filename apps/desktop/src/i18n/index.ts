33b1c346d2de07c630df57e3b86aa19216a225e7
Mariano Esteban <marianoesteban@users.noreply.github.com>
2024-10-17 05:54:35 -0300

    Add Spanish translations (#739)
    * Add Spanish locale
    
    * Add Spanish to settings

--- apps/desktop/src/i18n/index.ts
+++ apps/desktop/src/i18n/index.ts
@@ -4,22 +4,26 @@ import { initReactI18next } from 'react-i18next'
 import EN from '../../../../locales/en.json'
 import zhCN from '../../../../locales/zh-CN.json'
 import frFR from '../../../../locales/fr-FR.json'
+import es from '../../../../locales/es.json'
 
 export const locales = {
   en: 'English',
   cn: '简体中文',
   frFR: 'Français',
+  es: 'Español',
 }
 export const editorResources = {
   en: { translation: { ...EN.editor } },
   cn: { translation: { ...zhCN.editor } },
   frFR: { translation: { ...frFR.editor } },
+  es: { translation: { ...es.editor } },
 }
 
 export const resources = {
   en: { translation: { ...EN } },
   cn: { translation: { ...zhCN } },
   frFR: { translation: { ...frFR } },
+  es: { translation: { ...es } },
 } as const
 
 export async function i18nInit(options?: InitOptions) {
