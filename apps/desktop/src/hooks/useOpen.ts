193ddbbe8a21662a170acb6b286321c50f58bc71
drl990114 <drl990114@gmail.com>
2025-03-29 18:46:59 +0800

    feat: support open a file (#810)

--- apps/desktop/src/hooks/useOpen.ts
+++ apps/desktop/src/hooks/useOpen.ts
@@ -3,6 +3,8 @@ import { open } from '@tauri-apps/plugin-dialog'
 import { readDirectory } from '@/helper/filesys'
 import { useEditorStore } from '@/stores'
 import useOpenedCacheStore from '@/stores/useOpenedCacheStore'
+import { addExistingMarkdownFileEdit } from '@/services/editor-file'
+import { getFileContent } from '@/services/file-info'
 
 const useOpen = () => {
   const { setFolderData } = useEditorStore()
@@ -28,9 +30,24 @@ const useOpen = () => {
     openFolder(dir)
   }, [openFolder])
 
+  const openFile = useCallback(async () => {
+    const file = await open({ multiple: false, filters: [{ name: 'Markdown', extensions: ['md'] }] })
+    if (typeof file !== 'string') return
+    const fileContent = await getFileContent({ filePath: file })
+    if (!fileContent) return
+    const fileName = file.split('/').pop() || 'new-file.md'
+    await addExistingMarkdownFileEdit({
+      fileName,
+      content: fileContent,
+      path: file,
+    })
+  }
+  , [addRecentWorkspaces, setFolderData])
+
   return {
     openFolderDialog,
     openFolder,
+    openFile,
   }
 }
 
