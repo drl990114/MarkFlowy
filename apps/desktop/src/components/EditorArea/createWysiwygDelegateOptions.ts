0af20dc6c8ecb4611435bfefa88885cf50ebad85
drl990114 <drl990114@gmail.com>
2025-05-11 18:55:26 +0800

    feat: support AI generation within documents, create AI nodes through slash menu

--- apps/desktop/src/components/EditorArea/createWysiwygDelegateOptions.ts
+++ apps/desktop/src/components/EditorArea/createWysiwygDelegateOptions.ts
@@ -1,38 +1,73 @@
-import { sleep } from "@/helper"
-import { useEditorStore } from "@/stores"
-import type { CreateWysiwygDelegateOptions } from "rme"
+import { AIGenerateTextParams, getCurrentAISettingData } from '@/extensions/ai/aiProvidersService'
+import { aiGenerateTextRequest } from '@/extensions/ai/api'
+import useAiChatStore from '@/extensions/ai/useAiChatStore'
+import { sleep } from '@/helper'
+import { useEditorStore } from '@/stores'
 import { convertFileSrc } from '@tauri-apps/api/core'
 import { join } from '@tauri-apps/api/path'
 import { fetch } from '@tauri-apps/plugin-http'
+import type { CreateWysiwygDelegateOptions } from 'rme'
 
-export const createWysiwygDelegateOptions = (filePath?: string): CreateWysiwygDelegateOptions => ({
-  handleViewImgSrcUrl: async (url) => {
-    // Ensure asynchronous, returning directly will cause an infinite loop. about:https://github.com/drl990114/MarkFlowy/issues/340
-    await sleep(1)
+type AIOptions = NonNullable<CreateWysiwygDelegateOptions['ai']>
 
-    if (!url) return url
+export const createWysiwygDelegateOptions = (filePath?: string): CreateWysiwygDelegateOptions => {
+  const aiStoreState = useAiChatStore.getState()
+  const aiProviderModelsMap = aiStoreState.aiProviderModelsMap
+  let supportProviderInfosMap: AIOptions['supportProviderInfosMap'] = {}
 
-    if ((url.startsWith('http') || url.startsWith('https')) && !url.includes(location.origin)) {
-      try {
-        const response = await fetch(url, {
-          method: 'GET',
+  Object.entries(aiProviderModelsMap).map(([provider, models]) => {
+    supportProviderInfosMap[provider] = {
+      models: models,
+    }
+  })
+
+  return {
+    handleViewImgSrcUrl: async (url) => {
+      // Ensure asynchronous, returning directly will cause an infinite loop. about:https://github.com/drl990114/MarkFlowy/issues/340
+      await sleep(1)
+
+      if (!url) return url
+
+      if ((url.startsWith('http') || url.startsWith('https')) && !url.includes(location.origin)) {
+        try {
+          const response = await fetch(url, {
+            method: 'GET',
+          })
+
+          const blob = await response.blob()
+          const objectURL = URL.createObjectURL(blob)
+
+          return objectURL
+        } catch (error) {
+          return url
+        }
+      }
+
+      const dirPath = filePath || useEditorStore.getState().folderData?.[0]?.path
+      if (dirPath) {
+        const newUrl = await join(dirPath, url)
+        return convertFileSrc(newUrl)
+      }
+
+      return convertFileSrc(url)
+    },
+    ai: {
+      defaultSelectProvider: aiStoreState.aiProvider,
+      supportProviderInfosMap,
+      generateText: async (params) => {
+        const aiSettingData = getCurrentAISettingData()
+        const apiBase = aiSettingData.apiBase
+        const apiKey = aiSettingData.apiKey
+        const text = await aiGenerateTextRequest({
+          sdkProvider: params.provider as AIGenerateTextParams['sdkProvider'],
+          url: apiBase,
+          apiKey,
+          model: params.model,
+          text: params.prompt,
         })
 
-        const blob = await response.blob()
-        const objectURL = URL.createObjectURL(blob)
-
-        return objectURL
-      } catch (error) {
-        return url
-      }
-    }
-
-    const dirPath = filePath || useEditorStore.getState().folderData?.[0]?.path
-    if (dirPath) {
-      const newUrl = await join(dirPath, url)
-      return convertFileSrc(newUrl)
-    }
-
-    return convertFileSrc(url)
-  },
-})
+        return text
+      },
+    },
+  }
+}
