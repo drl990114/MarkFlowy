9de4b3f69edb0e3ae6bb099edea910d354d50e69
drl990114 <drl990114@gmail.com>
2025-07-13 22:17:33 +0800

    fix: adjust EditorCount positioning and opacity

--- apps/desktop/src/components/EditorArea/index.tsx
+++ apps/desktop/src/components/EditorArea/index.tsx
@@ -1,11 +1,11 @@
-import Editor from './Editor'
-import { Container } from './styles'
+import { FindReplace } from '@/components/EditorArea/editorToolBar/FindReplace'
 import { useEditorStore } from '@/stores'
 import { memo } from 'react'
+import Editor from './Editor'
 import EditorAreaTabs from './EditorAreaTabs'
-import { FindReplace } from '@/components/EditorArea/editorToolBar/FindReplace'
 import { EditorCount } from './editorToolBar/EditorCount'
 import { EmptyState } from './EmptyState'
+import { Container } from './styles'
 
 function EditorArea() {
   const { opened, activeId } = useEditorStore()
@@ -22,9 +22,9 @@ function EditorArea() {
     <Container className='w-full h-full'>
       <EditorAreaTabs />
       <FindReplace />
-        {opened.map((id) => {
-          return <Editor key={id} id={id} active={id === activeId} />
-        })}
+      {opened.map((id) => {
+        return <Editor key={id} id={id} active={id === activeId} />
+      })}
       <EditorCount />
     </Container>
   )
