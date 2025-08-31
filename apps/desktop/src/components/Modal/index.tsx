283d3296868f8c4a01a6766a4335190abbc5475f
drl990114 <drl990114@gmail.com>
2025-08-31 14:07:27 +0800

    feat: The file path is no longer displayed, but the update time is displayed #928

--- apps/desktop/src/components/Modal/index.tsx
+++ apps/desktop/src/components/Modal/index.tsx
@@ -1,10 +1,13 @@
 import { Confirm } from './Confirm'
+import { Info } from './Info'
 import { InputConfirm } from './InputConfirm'
 
 export * from './Confirm'
+export * from './Info'
 export * from './InputConfirm'
 
 export default {
   Confirm: Confirm,
   InputConfirm: InputConfirm,
+  Info: Info
 }
