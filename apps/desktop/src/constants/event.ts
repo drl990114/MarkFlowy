e0687d12f4f0571fc052a47e3fed27d33e44e3c7
drl990114 <drl990114@gmail.com>
2025-11-01 01:04:38 +0800

    feat: support app hide shortcut key #965

--- apps/desktop/src/constants/event.ts
+++ apps/desktop/src/constants/event.ts
@@ -1,6 +1,7 @@
 enum EVENT {
   app_about = 'app_about',
   app_save = 'app_save',
+  app_openFolder = 'app_openFolder',
   app_openSetting = 'app_openSetting',
   app_hide = 'app_hide',
   app_closeCurrentEditorTab = 'app_closeCurrentEditorTab'
