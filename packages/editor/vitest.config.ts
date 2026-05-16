a4491382c68e5733816beb2903071a9735243b1f
drl990114 <drl990114@gmail.com>
2026-05-16 16:36:09 +0800

    fix: test ci

--- packages/editor/vitest.config.ts
+++ packages/editor/vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(() => ({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: ['./__test__/setup-vitest.ts'],
+    setupFiles: ['./src/editor/test/setup-vitest.ts'],
   },
   coverage: {
     reporter: ['text', 'json', 'html']
